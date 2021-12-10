401f159b019fa7edabd644eb4d074e6e056aeb34
NJH98 <a2372557@naver.com>
2021-12-10 17:33:06 +0900

    no message

--- Netfilx/src/main/java/com/it/springboot/Controller/ConController.java
+++ Netfilx/src/main/java/com/it/springboot/Controller/ConController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ConController {
     
-    @RequestMapping("/main")
-    public String main(){
+    @RequestMapping("/play1")
+    public String play1(){
         return "main";
 	}
 
